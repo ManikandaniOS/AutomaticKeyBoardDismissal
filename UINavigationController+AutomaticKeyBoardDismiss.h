3885fd6474006680b599d8c70b01735ad9c33cab
satheeshwaran <satheesh4590@gmail.com>
2013-07-26 11:50:16 +0530

    Added initial files

---
+++ UINavigationController+AutomaticKeyBoardDismiss.h
@@ -0,0 +1,15 @@
+//
+//  UINavigationController+AutomaticKeyBoardDismiss.h
+//  iPitch V2
+//
+//  Created by Satheeshwaran on 7/25/13.
+//  Copyright (c) 2013 Cognizant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationController (AutomaticKeyBoardDismiss)
+
+- (BOOL)disablesAutomaticKeyboardDismissal;
+
+@end
---
+++ UINavigationController+AutomaticKeyBoardDismiss.m
@@ -0,0 +1,18 @@
+//
+//  UINavigationController+AutomaticKeyBoardDismiss.m
+//  iPitch V2
+//
+//  Created by Satheeshwaran on 7/25/13.
+//  Copyright (c) 2013 Cognizant. All rights reserved.
+//
+
+#import "UINavigationController+AutomaticKeyBoardDismiss.h"
+
+@implementation UINavigationController (AutomaticKeyBoardDismiss)
+
+- (BOOL)disablesAutomaticKeyboardDismissal
+{
+    return NO;
+}
+
+@end
